54124a55516d324383737a9dccfab7f82c193a1a
longobucco <longobuccofbc@gmail.com>
2024-10-13 11:47:54 -0300

    creation of the home page and game search field

---
+++ src/app/game/search/[title]/page.tsx
@@ -0,0 +1,42 @@
+import { Container } from "@/components/container";
+import { GameCard } from "@/components/gamecard";
+import { Input } from "@/components/input";
+import { GameProps } from "@/utils/types/game";
+
+
+async function getData(title: string) {
+
+    try {
+        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&title=${title}`)
+        return res.json()
+    } catch (err) {
+        return null;
+    }
+
+}
+
+
+export default async function Page({ params: { title } }: {
+    params: { title: string }
+}) {
+
+    const games: GameProps[] = await getData(title)
+
+    return (
+        <main>
+            <Container>
+                <Input />
+                <h1 className="font-bold text-xl mt-8 mb-5">veja o que encontramos na nossa base</h1>
+                {!games && (
+                    <p>jogo não encontrado...!</p>
+                )}
+                <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    {games && games.map((item) => (
+                        <GameCard key={item.id} data={item} />
+                    )
+                    )}
+                </section>
+            </Container>
+        </main>
+    )
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { LiaGamepadSolid } from "react-icons/lia"
+
+export function Header() {
+    return (
+        <header className="w-full h-28 bg-slate-100 text-black px-2">
+            <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+                <nav className="flex justify-center items-center gap-4">
+                    <Link href="/" >
+                        <Image
+                            src="/assets/logo.svg"
+                            alt="logo"
+                            quality={100}
+                            priority={true}
+                            width={168}
+                            height={39}
+                            className="w-full"
+                        />
+                    </Link>
+
+                    <Link href="Games">
+                        Games
+                    </Link>
+
+                    <Link href="/profile">
+                        Perfil
+                    </Link>
+                </nav>
+
+                <div className="hidden sm:flex">
+                    <Link href="/profile">
+                        <LiaGamepadSolid size={34}/>
+                    </Link>
+                </div>
+
+            </div>
+        </header>
+    )
+}

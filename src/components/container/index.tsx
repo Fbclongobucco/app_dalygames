6030d8964938f4b6a5d58fe25e2d626e379818e7
longobucco <longobuccofbc@gmail.com>
2024-10-15 17:52:57 -0300

    margin refactoring on mobile devices.

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function Container({children}: {children: ReactNode}){
     return(
-        <div className="max-w-screen-xl m-auto">
+        <div className="max-w-screen-xl sm:m-auto sm mx-2">
         {children}
       </div>
     )
